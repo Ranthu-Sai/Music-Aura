434c1728ecc31a0abb88d7d7a6a71386bc48e50a
Sai Ranthu <sai.ranthu@sasi.ac.in>
2026-09-03 11:32:10 +0530

    Fix app crashing in startup

--- android/app/src/main/java/com/musicaura/MainApplication.kt
+++ android/app/src/main/java/com/musicaura/MainApplication.kt
@@ -4,13 +4,11 @@ import android.content.Context
 import com.facebook.react.PackageList
 import com.facebook.react.ReactApplication
 import com.facebook.react.ReactHost
-import com.facebook.react.ReactInstanceManager
+import com.facebook.react.ReactNativeApplicationEntryPoint.loadReactNative
 import com.facebook.react.ReactNativeHost
 import com.facebook.react.ReactPackage
-import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.load
 import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
 import com.facebook.react.defaults.DefaultReactNativeHost
-import com.facebook.soloader.SoLoader
 
 class MainApplication : Application(), ReactApplication {
 
@@ -35,27 +33,6 @@ class MainApplication : Application(), ReactApplication {
 
   override fun onCreate() {
     super.onCreate()
-    SoLoader.init(this, false)
-    if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
-      // If you opted-in for the New Architecture, we load the native entry point for this app.
-      load()
-    }
-    initializeFlipper(this, reactNativeHost.reactInstanceManager)
-  }
-}
-
-private fun initializeFlipper(context: Context, reactInstanceManager: ReactInstanceManager) {
-  if (BuildConfig.DEBUG) {
-    try {
-      val flipperClass = Class.forName("com.facebook.flipper.ReactNativeFlipper")
-      val method = flipperClass.getMethod(
-        "initializeFlipper",
-        Context::class.java,
-        ReactInstanceManager::class.java
-      )
-      method.invoke(null, context, reactInstanceManager)
-    } catch (e: Exception) {
-      e.printStackTrace()
-    }
+    loadReactNative(this)
   }
 }
